533d3c45b01474c886487e0285a54e81fa2abf9b
jantmi <jant_mi@outlook.com>
2014-10-10 11:34:33 -0400

    Reverse a String

---
+++ Reverse a String/ReverseAString/ReverseAString/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReverseAString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the string you wish to reverse below:");
+            string originalString = Console.ReadLine();
+            Console.WriteLine("The string you want to reverse is " + originalString);
+            string reversedString = new string(originalString.Reverse().ToArray());
+            Console.WriteLine("The reversed string is " + reversedString);
+            Console.Read();
+        }
+    }
+}
